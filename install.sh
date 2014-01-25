a57356427ba07388888459d7c2ab4ea13de96542
Fan Jiang <fanjiang@thoughtworks.com>
2014-01-25 23:54:05 +0800

    use script to do auto installing

---
+++ install.sh
@@ -0,0 +1,23 @@
+printf "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">
+<plist version=\"1.0\">
+<dict>
+    <key>Label</key>
+    <string>net.torchz.hotdog</string>
+    <key>ThrottleInterval</key>
+    <integer>1</integer>
+    <key>WorkingDirectory</key>
+    <string>" > hotdog.plist
+pwd | tr -d '\n' >> hotdog.plist
+printf "</string>
+    <key>ProgramArguments</key>
+      <array>
+      <!--<string>/Users/twer/hotdog/hotdog.sh</string>-->
+        <string>./hotdog.sh</string>
+      </array>
+    <key>StartInterval</key>
+    <integer>2</integer>
+</dict>
+</plist>
+" >> hotdog.plist
+chmod +x hotdog.plist
